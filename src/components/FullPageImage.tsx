23b1772900bb9c5fb6f6fa41588265a56f719ce4
Marek <marekreid@gmail.com>
2023-09-03 22:00:48 +0200

    latest test

--- src/components/FullPageImage.tsx
+++ src/components/FullPageImage.tsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { useProducts } from "../../contexts/ProductContexts";
 
 interface FullScreenImageProps {
   imageUrl: string;
 }
 
-const FullScreenImage: React.FC<FullScreenImageProps> = ({ imageUrl }) => (
-  <div
-    className="w-full h-[70vh] bg-cover"
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-      backgroundSize: "cover", // Add this line to maintain aspect ratio
-    }}
-  ></div>
-);
+const FullScreenImage: React.FC<FullScreenImageProps> = ({ imageUrl }) => {
+  const { products } = useProducts();
+  const image = products[0].image;
+  return (
+    <div
+      className="w-full h-[70vh] bg-cover"
+      style={{
+        backgroundImage: `url(${image})`,
+        backgroundSize: "cover", // Add this line to maintain aspect ratio
+      }}
+    ></div>
+  );
+};
 
 export default FullScreenImage;
